5d7a4ea33573e20266802fc5ed7cf14d0300c9d8
Drifter <sgloom@gmail.com>
2023-06-20 16:29:27 +0500

    New config structure: snmp and hubs

--- internal/interfaceadapters/config/file/config_test.go
+++ internal/interfaceadapters/config/file/config_test.go
@@ -3,6 +3,7 @@ package file
 import (
 	"testing"
 
+	"github.com/gosnmp/gosnmp"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,10 +13,19 @@ func TestFileConfigService_Read(t *testing.T) {
 	conf := configService.GetConfig()
 
 	assert.Equal(t, "user:password@tcp(127.0.0.1:3306)/database1?charset=utf8mb4&parseTime=True&loc=Local", conf.Hubs[0].Dsn)
-	assert.Equal(t, "storage1", conf.Hubs[0].Hubname)
-	assert.Equal(t, "user:password@tcp(127.0.0.1:5432)/database2?sslmode=disable", conf.Hubs[1].Dsn)
-	assert.Equal(t, "storage2", conf.Hubs[1].Hubname)
+	assert.Equal(t, "hub1", conf.Hubs[0].Hubname)
+	assert.Equal(t, "127.0.0.1", conf.Hubs[0].Snmp.Address)
+	assert.Equal(t, uint16(161), conf.Hubs[0].Snmp.Port)
+	assert.Equal(t, "public", conf.Hubs[0].Snmp.Community)
+	assert.Equal(t, gosnmp.SnmpVersion(0x1), conf.Hubs[0].Snmp.Version)
+
+	assert.Equal(t, "user:password@tcp(10.0.0.1:5432)/database2?sslmode=disable", conf.Hubs[1].Dsn)
+	assert.Equal(t, "hub2", conf.Hubs[1].Hubname)
+	assert.Equal(t, "10.0.0.1", conf.Hubs[1].Snmp.Address)
+	assert.Equal(t, uint16(161), conf.Hubs[1].Snmp.Port)
+	assert.Equal(t, "public", conf.Hubs[1].Snmp.Community)
+	assert.Equal(t, gosnmp.SnmpVersion(0x1), conf.Hubs[1].Snmp.Version)
+
 	assert.Equal(t, "./templates/", conf.Http.TemplatesDir)
 	assert.Equal(t, ":8080", conf.Http.ListenAddr)
-
 }
